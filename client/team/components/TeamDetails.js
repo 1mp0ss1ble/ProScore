3c82803fd8127ff08957067bb352ac479d5c3f2f
askarisback@gmail.com <askarisback@gmail.com>
2017-06-28 00:10:44 +0500

    upd match details

--- client/team/components/TeamDetails.js
+++ client/team/components/TeamDetails.js
@@ -4,11 +4,30 @@ export default function({ model }) {
   console.log(model);
   return (
     <div>
-      <h3>Team: {model.desc}{' '}
+      {model.logo && <img src={model.logo} widht="50px" height="50px" />}
+      <h3>{model.desc}</h3>
+        {model.info &&
+          <p>Info: {' '}
+            <span className="text-info">{model.info}</span>
+          </p>
+        }
 
-        </h3>
+
+        {model.city &&
+          <p>City: {' '}
+            <span className="text-info">{model.city}</span>
+          </p>
+        }
+
+        {model.sponsor &&
+          <p>Sponsor: {' '}
+            <span className="text-info">{model.sponsor}</span>
+          </p>
+        }
+
+
         {model.link &&
-          <p>Webiste: <a href={model.link} target="_blank">
+          <p>Webiste: <a className="text-info" href={model.link} target="_blank">
             {model.link}</a>
           </p>
         }
